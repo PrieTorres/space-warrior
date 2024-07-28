3df88b2e5a9ec1af77f761ded8363b87f88c237f
Priscila <priscila.paula@clinicorp.com>
2024-07-28 17:57:20 -0300

    game over rank input improvement

--- src/Components/NameInput/NameInput.jsx
+++ src/Components/NameInput/NameInput.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState, useRef } from "react";
+import { useEffect, useState, useRef } from "react";
 import { BigLetter } from "../BigLetter/BigLetter";
 import styles from "./NameInput.module.scss";
 
@@ -6,32 +6,37 @@ export const NameInput = ({ limit = 10, onChange }) => {
   const [name, setName] = useState("aaa");
   const inputRef = useRef(null);
 
-  const nameInputHandler = useCallback((e) => {
-    const letter = e.key;
-    if (letter === "Backspace") {
-      return setName(`${name.substring(0, (name.length - 1))}`)
-    }
-    if (name.length >= limit) return;
-    if (letter.length > 1) return;
-    if (!letter.match(/\w/ig)) return;
-
-    setName(name + letter);
-  }, [name, setName, limit]);
-
   useEffect(() => {
-    window.addEventListener("keydown", nameInputHandler);
-
-    return () => window.removeEventListener("keydown", nameInputHandler);
-  }, [nameInputHandler]);
-
-  useEffect(() => {
-    onChange(name)
+    onChange(name);
   }, [name, onChange]);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.onpaste = (e) => e.preventDefault();
+    }
+  }, [])
 
   return (
     <div className={styles.container}>
-      <input ref={inputRef} type="text" value={name} onChange={nameInputHandler} autoFocus style={{ display: "none" }} />
-      <div style={{ display: "flex", gap: 6 }} onClick={() => inputRef?.current?.focus()}>
+      <input
+        ref={inputRef}
+        type="text"
+        value={name}
+        autoFocus={true}
+        style={{ opacity: 0, width: 0 }}
+        onChange={(e) => {
+          const val = e?.target?.value;
+          if (val?.length > limit) return;
+          if (val?.match(/\s+/ig)) return;
+          setName(val);
+        }}
+      />
+      <div
+        role="button"
+        className="prevent-select"
+        style={{ display: "flex", gap: 6 }}
+        onClick={() => inputRef?.current?.focus()}
+      >
         {name.split("").map((letter, i) => (
           <BigLetter
             key={`big-letter-${i}-${letter}`}
