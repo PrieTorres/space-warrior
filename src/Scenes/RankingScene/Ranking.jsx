27399b1fe1d00f5b6291cff4534ee80d2a34ad92
Priscila <priscilatorres2023@gmail.com>
2024-07-29 11:02:37 -0300

    fix: rank display

--- src/Scenes/RankingScene/Ranking.jsx
+++ src/Scenes/RankingScene/Ranking.jsx
@@ -1,4 +1,4 @@
-import { useContext, useRef, useEffect } from "react";
+import { useContext, useRef, useEffect, useCallback } from "react";
 import style from "./Ranking.module.scss"
 import { GameContext } from "../../contexts/GameContext";
 
@@ -27,22 +27,25 @@ export const Ranking = () => {
     })
   }, [gameDispatch]);
 
-  const getRaknDisplay = (name, points, containerWidth, padding = 0) => {
+  const getRankDisplay = useCallback((name, points, padding = 0) => {
+    const containerWidth = (container.current?.offsetWidth ?? window.innerWidth) - padding * 2;
     let rankDisplay = `${name}`;
-    let maxLength = `${points}`.length + ((containerWidth - padding * 2) / 12);
+    let maxLength = (containerWidth / 5.6) - `${points}`.length - rankDisplay.length;
+
     for (let i = name.length; i < maxLength; i++) {
       rankDisplay += ".";
     }
+
     rankDisplay += points;
 
     return rankDisplay
-  };
+  }, [container.current]);
 
   return (
     <div className={`${style['container']}`}>
-      <div ref={container} style={{ padding: 20 }}>
+      <div ref={container} style={{ padding: 20, width: "100%", fontSize: 14,  }}>
         {ranks.map((rankData, i) => (
-          <div key={i}>{getRaknDisplay(rankData.rankName, rankData.points, container.current ? container.current.offsetWidth : window.innerWidth, 20)}</div>
+          <div key={i}>{getRankDisplay(rankData.rankName, rankData.points, 20)}</div>
         ))}
       </div>
     </div>
