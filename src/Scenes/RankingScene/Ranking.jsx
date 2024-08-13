4a798a7dff606c770fc9cd768d0e998a86ea8de6
Priscila <priscilatorres2023@gmail.com>
2024-08-12 21:14:50 -0300

    up

--- src/Scenes/RankingScene/Ranking.jsx
+++ src/Scenes/RankingScene/Ranking.jsx
@@ -7,6 +7,8 @@ export const Ranking = () => {
   const { gameState, gameDispatch } = useContext(GameContext);
   const { ranks } = gameState;
   const container = useRef(null);
+  const pointDiv = useRef(null);
+  const nameDiv = useRef(null);
 
   const getRanking = useCallback(() => {
     return new Promise(async (resolve) => {
@@ -34,22 +36,36 @@ export const Ranking = () => {
       return "";
     }
 
-    const containerWidth = (container.current?.offsetWidth ?? window.innerWidth) - padding * 2;
+    const divSize = 5.61//( 5.61 ?? pointDiv.current?.clientWidth) ?? 5.6;
+    const containerWidth = (container.current?.clientWidth ?? window.innerWidth) - padding * 2;
+
     let rankDisplay = `${name}`;
-    let maxLength = (containerWidth / 5.6) - `${points}`.length - rankDisplay.length;
+    const pointsStr = `${points}`;
+
+    const a = [];
+
+    if(nameDiv.current) nameDiv.current.innerHTML = `${rankDisplay}${pointsStr}`;
+    const sizeNamePoints = (pointsStr.length + rankDisplay.length) * 5.61  //nameDiv?.current?.clientWidth ?? pointsStr.length + rankDisplay.length;
+
+    let maxLength = (containerWidth / divSize) - sizeNamePoints;
 
     for (let i = name?.length; i < maxLength; i++) {
       rankDisplay += ".";
+      a.push(".")
     }
+
+    let teste = (<div><span>{name}</span>{a.map((b) => <span>{b}</span>)}<span>{points}</span></div>)
 
     rankDisplay += points;
 
-    return rankDisplay
+    return teste
   }, [container.current]);
 
   return (
     <div className={`${style['container']}`}>
       <div ref={container}>
+      <span className={`${style['point-div']}`} ref={pointDiv}>.</span>
+      <span className={`${style['point-div']}`} ref={nameDiv}></span>
         {ranks.sort((a, b) => b?.points - a?.points).map((rankData, i) => (
           <div key={i}>{getRankDisplay(rankData.rankName, rankData.points, 20)}</div>
         ))}
