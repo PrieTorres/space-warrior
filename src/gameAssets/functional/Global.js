f491d3f294c2b682bea54e3f2acdbe8fad5ef3a5
Priscila <priscila.torres@clinicorp.com>
2025-02-25 20:48:46 -0300

    feat: levels adjusts

--- src/gameAssets/functional/Global.js
+++ src/gameAssets/functional/Global.js
@@ -6,53 +6,63 @@ export const CONST = {
 
 export const LEVELS_DATA = [
   {
+    level: 1,
     respawnAsteroid: 2500,
     typesAsteroids: [1, 2, 3],
     minPoints: 0,
   },
   {
+    level: 2,
     respawnAsteroid: 2000,
     typesAsteroids: [1, 2, 3, 4],
     minPoints: 50,
   },
   {
+    level: 3,
     respawnAsteroid: 1500,
     typesAsteroids: [1, 2, 4],
     minPoints: 120,
   },
   {
+    level: 4,
     respawnAsteroid: 1000,
     typesAsteroids: [3, 4],
     minPoints: 190,
   },
   {
+    level: 5,
     respawnAsteroid: 750,
     typesAsteroids: [1, 3, 4],
     minPoints: 280,
   },
   {
+    level: 6,
     respawnAsteroid: 750,
-    typesAsteroids: [1, 3, 4, 5],
+    typesAsteroids: [1, 3, 4, 5, 7],
     minPoints: 400,
   },
   {
+    level: 7,
     respawnAsteroid: 750,
-    typesAsteroids: [1, 3, 4, 6],
+    typesAsteroids: [1, 3, 4, 6, 7],
     minPoints: 540,
   },
   {
-    respawnAsteroid: 650,
-    typesAsteroids: [5, 6],
+    level: 8,
+    respawnAsteroid: 700,
+    typesAsteroids: [5, 6, 7, 8, 9],
     minPoints: 700,
   },
   {
+    level: 9,
     respawnAsteroid: 550,
-    typesAsteroids: [2, 4, 5, 6],
+    typesAsteroids: [2, 4, 5, 6, 8],
     minPoints: 900,
   },
   {
+    level: 10,
     respawnAsteroid: 350,
-    typesAsteroids: [1, 2, 3, 4, 5, 6],
+    typesAsteroids: [1, 2, 3, 4, 5, 6, 7, 8, 9],
     minPoints: 1200,
   },
 ];
